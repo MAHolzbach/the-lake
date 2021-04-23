10c2161a6722412c2f5aa3fecaeaeb080f0fe25a
Michael Holzbach <michael.holzbachelinvar.de@MacBookPro152-michaelholzbachelinvarde.local>
2021-04-23 17:52:00 +0200

    Will do for now

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'Project Title',
+      title: 'The Lake',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
---
+++ src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => (
+  <div>
+    <h1 className="landing-title">The Lake</h1>
+    <h3 className="landing-subtitle">
+      Welcome to New York&apos;s premier outdoor, aquatic adventure destination!
+    </h3>
+  </div>
+)
+
+export default Services
