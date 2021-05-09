067d2fbc793aac5bdfecc161d061bdff811b4400
Michael Holzbach <michael.holzbachelinvar.de@MacBookPro152-michaelholzbachelinvarde.local>
2021-05-09 09:12:46 +0200

    Styling

--- src/components/BoatDetails.jsx
+++ src/components/BoatDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, NavLink } from 'react-router-dom'
 import axios from 'axios'
 
 import breeze from '../images/breeze.png'
@@ -43,12 +43,20 @@ const BoatDetails = () => {
     <div className={`vehicle-details ${boat}`}>
       <div className="vehicle-details__content">
         <div className="vehicle-details__data">
-          <h1 className="vehcile-details__title">{boatData.renderName}</h1>
+          <h1 className="vehicle-details__title">{boatData.renderName}</h1>
+          <p className="vehicle-details__class">{boatData.class}</p>
+          <p className="vehicle-details__price">
+            {boatData.price}{' '}
+            <span className="vehicle-details__price__text">/day</span>
+          </p>
         </div>
         <div className="vehicle-details__image-wrapper">
           <img className="vehicle-details__image" src={images[boat]} />
         </div>
       </div>
+      <NavLink to="/book" className="vehicle-details__navlink">
+        <button className="vehicle-details__button">Rent</button>
+      </NavLink>
     </div>
   )
 }
