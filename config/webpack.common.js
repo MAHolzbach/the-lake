eb217e949c88debbf9cef7e84ff8ab0b24100582
Michael Holzbach <michael.holzbach@elinvar.de>
2022-08-10 10:49:22 +0200

    Update dependencies

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const PrettierPlugin = require('prettier-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 const paths = require('./paths')
@@ -54,7 +53,6 @@ module.exports = {
     }),
 
     // Prettier configuration
-    new PrettierPlugin(),
     new Dotenv({ path: paths.env }),
   ],
 
